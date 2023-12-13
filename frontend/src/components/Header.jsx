129376fae8c05086dc8663da673aa65fbc05bc89
itric <itric46@gmail.com>
2023-12-13 14:02:23 +0545

    register page ui done

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,20 +1,26 @@
-import { Button, Image, useColorMode } from "@chakra-ui/react";
+import { Button, Flex, Image, useColorMode } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+  const navigate = useNavigate();
   const { colorMode, toggleColorMode } = useColorMode();
   return (
     <>
-      <Image
-        cursor={"pointer"}
-        alt="logo"
-        w={10}
-        src={
-          colorMode == "dark"
-            ? "/icons8-light-mode-78.png"
-            : "/icons8-dark-mode-50.png"
-        }
-        onClick={toggleColorMode}
-      />
+      <Flex justify={"flex-end"} gap={5} margin={"9px 9px 0 0"}>
+        <Image
+          cursor={"pointer"}
+          alt="logo"
+          w={10}
+          src={
+            colorMode == "dark"
+              ? "/icons8-light-mode-78.png"
+              : "/icons8-dark-mode-50.png"
+          }
+          onClick={toggleColorMode}
+        />
+        <Button onClick={() => navigate("/register")}>Sign Up</Button>
+        <Button>Sign in</Button>
+      </Flex>
     </>
   );
 }
