423e4f931cefa0979d9b2de025c85034db430856
itric <itric46@gmail.com>
2023-12-13 13:31:42 +0545

    light / dark mode image

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,20 @@
-import { Button, useColorMode } from "@chakra-ui/react";
+import { Button, Image, useColorMode } from "@chakra-ui/react";
 
 function Header() {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
     <>
-      <Button onClick={toggleColorMode}>hi</Button>
+      <Image
+        cursor={"pointer"}
+        alt="logo"
+        w={10}
+        src={
+          colorMode == "dark"
+            ? "/icons8-light-mode-78.png"
+            : "/icons8-dark-mode-50.png"
+        }
+        onClick={toggleColorMode}
+      />
     </>
   );
 }
