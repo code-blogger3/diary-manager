497d87f8ebf78bb6c32160133e8a8831782a5742
itric <itric46@gmail.com>
2023-12-14 12:02:17 +0545

    backend folder structure, packages install, package json modifications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -35,7 +35,7 @@ function App() {
   ]);
   return (
     <>
-      <RouterProvider router={BrowserRoutes} />;
+      <RouterProvider router={BrowserRoutes} />
     </>
   );
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,25 +1,29 @@
 import { Button, Flex, Image, useColorMode } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
+import { AiFillHome } from "react-icons/ai";
 
 function Header() {
   const navigate = useNavigate();
   const { colorMode, toggleColorMode } = useColorMode();
   return (
     <>
-      <Flex justify={"flex-end"} gap={5} margin={"9px 9px 0 0"}>
-        <Image
-          cursor={"pointer"}
-          alt="logo"
-          w={10}
-          src={
-            colorMode == "dark"
-              ? "/icons8-light-mode-78.png"
-              : "/icons8-dark-mode-50.png"
-          }
-          onClick={toggleColorMode}
-        />
-        <Button onClick={() => navigate("/register")}>Sign Up</Button>
-        <Button onClick={() => navigate("/login")}>Sign in</Button>
+      <Flex justify={"space-between"} margin={"9px 9px 0 9px"}>
+        <AiFillHome size={24} onClick={() => navigate("/")} />
+        <Flex gap={5}>
+          <Image
+            cursor={"pointer"}
+            alt="logo"
+            w={10}
+            src={
+              colorMode == "dark"
+                ? "/icons8-light-mode-78.png"
+                : "/icons8-dark-mode-50.png"
+            }
+            onClick={toggleColorMode}
+          />
+          <Button onClick={() => navigate("/register")}>Sign Up</Button>
+          <Button onClick={() => navigate("/login")}>Sign in</Button>
+        </Flex>
       </Flex>
     </>
   );
