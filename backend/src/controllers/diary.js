dbd3847ced2c80a76ef5aab7eef5453ed789a2e7
itric <itric46@gmail.com>
2023-12-18 16:21:16 +0545

    update diary api complete

--- backend/src/controllers/diary.js
+++ backend/src/controllers/diary.js
@@ -2,6 +2,7 @@ import { UserModel } from "../models/user.js";
 import { DiaryModel } from "../models/diary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { v2 as cloudinary } from "cloudinary";
 import { ApiError } from "../utils/ApiError.js";
 
 const sendUserDiary = asyncHandler(async (req, res) => {
@@ -18,7 +19,10 @@ const sendUserDiary = asyncHandler(async (req, res) => {
 const postDiary = asyncHandler(async (req, res) => {
   const { userID, title, text, category } = req.body;
   let img = req.body.img;
-
+  if (img) {
+    const uploadedResponse = await cloudinary.uploader.upload(img);
+    img = uploadedResponse.secure_url;
+  }
   const diary = new DiaryModel({
     title,
     diaryImgUrl: img,
@@ -39,4 +43,50 @@ const postDiary = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, "diary created successfully", newDiary));
 });
 
-export { sendUserDiary, postDiary };
+const updateDiary = asyncHandler(async (req, res) => {
+  const { diaryID } = req.params;
+  const { title, text, category } = req.body;
+  let img = req.body.img;
+
+  const diary = await DiaryModel.findById(diaryID);
+
+  if (!diary) {
+    throw new ApiError(401, "diary not found");
+  }
+
+  if (img) {
+    if (diary.diaryImgUrl) {
+      await cloudinary.uploader.destroy(
+        diary.diaryImgUrl.split("/").pop().split(".")[0]
+      );
+    }
+
+    const uploadedResponse = await cloudinary.uploader.upload(img);
+    img = uploadedResponse.secure_url;
+  }
+
+  diary.title = title || diary.title;
+  diary.text = text || diary.text;
+  diary.diaryImgUrl = img || diary.diaryImgUrl;
+  diary.category = category || diary.category; //not sure about this line
+  const updatedDiary = await DiaryModel.findByIdAndUpdate(
+    diaryID,
+    {
+      $set: {
+        title: diary.title,
+        text: diary.text,
+        diaryImgUrl: diary.diaryImgUrl,
+        category: diary.category,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, "diary updated successfully", updatedDiary));
+});
+
+export { sendUserDiary, postDiary, updateDiary };
--- backend/src/routes/diary.js
+++ backend/src/routes/diary.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { postDiary, sendUserDiary } from "../controllers/diary.js";
+import { postDiary, sendUserDiary, updateDiary } from "../controllers/diary.js";
 
 const router = express.Router();
 
 router.get("/:userID", sendUserDiary);
+router.put("/:diaryID", updateDiary);
 router.post("/", postDiary);
 
 export { router as diaryRouter };
